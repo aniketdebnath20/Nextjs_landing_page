6021fd086efc8bd580432b4cda2fdc96ba1c0de2
aniket debnath <debnathaniket41@gmail.comgit config --global user.email debnathaniket41@gamil.comclear>
2025-03-11 09:53:37 +0530

    add all file

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
+import { Brand, CTA } from '@/components'
+import { Blog, Features, Header, Possibility, WhatGPT3 } from '@/containers'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-
+      <div className="App">
+        <div className="gradient__bg">
+          {/* <Navbar /> */}
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        {/* <Footer /> */}
+      </div>
 
 
 
---
+++ components/brand/Brand.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import Image from 'next/image';
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <Image src={google} />
+    </div>
+    <div>
+      <Image src={slack} />
+    </div>
+    <div>
+      <Image src={atlassian} />
+    </div>
+    <div>
+      <Image src={dropbox} />
+    </div>
+    <div>
+      <Image src={shopify} />
+    </div>
+  </div>
+);
+
+export default Brand;
